18dc5ed231bf88bea0dc7ff4a9f489356c8175c3
Daniel Curcio <crcdnl02e28m208a@studenti.unical.it>
2024-10-29 06:52:08 +0100

    BEE-26 - Working login between frontend-backend

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -4,25 +4,44 @@ import { Observable, of, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
+import { Credentials } from '../models/credentials';
+import { AuthToken } from '../models/auth_token';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private isLoggedIn = false;
-  private loginUrl = `${environment.API_ENDPOINT}/login`;
+
+  private token?: string;
+  private role?: string;
 
 
   constructor(private http: HttpClient) {}
 
   login(email: string, password: string): Observable<boolean> {
-    // Simulate an API call
-    if (email === 'test@example.com' && password === 'password123') {
-      this.isLoggedIn = true;
-      return of(true); // Simulate a successful login
-    } else {
-      return of(false); // Simulate a failed login
-    }
+    const creds: Credentials = { email, password };
+    
+    return this.http.post<AuthToken>(`${environment.API_ENDPOINT}/login`, creds)
+      .pipe(
+        map((authToken: AuthToken) => {
+          this.token = authToken.token;
+          this.role = authToken.role;
+          this.isLoggedIn = true;
+          return true;
+        }),
+        // catch 3 types of errors (unauthorized(401), not-found(404) and generic server error)
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 401) {
+            return of(false);
+          }
+          if (error.status === 404) {
+            return of(false);
+          }
+          return throwError(error);
+        })
+      );
   }
 
   logout() {
