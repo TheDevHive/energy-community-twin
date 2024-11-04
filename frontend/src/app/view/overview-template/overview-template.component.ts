8bc96d06762877dfddf6b75a21bba2c7b89b404f
itsmexp <galardo.emanuele@gmail.com>
2024-11-04 15:56:10 +0100

    BEE-5: Refactoring Community Page

--- frontend/src/app/view/overview-template/overview-template.component.ts
+++ frontend/src/app/view/overview-template/overview-template.component.ts
@@ -70,7 +70,7 @@ export class OverviewTemplateComponent implements OnInit {
     this.commService.getCommunity(communityID).subscribe(
       (community: Community) => {
         this.currentCommunity = community;
-        this.name = "Community name: " + community.name;
+        this.name = community.name;
   
         if (buildingID == null) {
           this.loadAllBuildings(communityID);
@@ -102,7 +102,6 @@ export class OverviewTemplateComponent implements OnInit {
   }
   
   public loadApartments(buildingID: number) {
-    this.name = 'Building ID: ' + buildingID;
     this.buildingService.getBuilding(buildingID).subscribe(
       (building: Building) => {
         this.currentBuilding = building;
