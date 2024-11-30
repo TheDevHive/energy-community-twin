fe33028fbfcb31d1efd622aa1fbb1e3bc57ed577
Ilaria <ila1402@icloud.com>
2024-11-30 18:23:03 +0100

    BEE-110 BEE-111 BEE-112 BEE-113 BEE-114 DONE

--- frontend/src/app/view/SHARED/energy-reports/energy-reports.component.ts
+++ frontend/src/app/view/SHARED/energy-reports/energy-reports.component.ts
@@ -146,8 +146,6 @@ startNewSimulation() {
   modalRef.result.then(
     (result) => {
       if (result) {
-        console.log('Simulation data:', result);
-
         // Fetch new reports and update the UI
         this.reportService.getReports(this.refUUID).subscribe({
           next: (reports) => {
