e4aaf2d026d864b92751ea9b4c6cefa1b5189666
SalvatoreBia <salvatorebia02@gmail.com>
2024-12-03 20:11:32 +0100

    Rimosso MailSettings.java dal tracciamento e aggiornato il .gitignore

--- backend/src/main/java/com/example/demo/mail/MailSettings.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.mail;
-
-public class MailSettings
-{
-    public static final String mail = "";
-    public static final String password = "";
-    public static final String server = "";
-}
