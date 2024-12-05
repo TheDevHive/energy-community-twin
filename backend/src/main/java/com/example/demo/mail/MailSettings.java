f6d210715003a63738a8f946850ade5a0c97989d
Ilaria <ila1402@icloud.com>
2024-12-05 14:18:07 +0100

    BEE-130 fix

--- backend/src/main/java/com/example/demo/mail/MailSettings.java
+++ backend/src/main/java/com/example/demo/mail/MailSettings.java
@@ -2,7 +2,7 @@ package com.example.demo.mail;
 
 public class MailSettings
 {
-    public static final String mail = "thedevhivestaff@gmail.com";
-    public static final String password = "tumo vbuu ggmt mpic";
+    public static final String mail = "";
+    public static final String password = "";
     public static final String server = "smtp.gmail.com";
 }
