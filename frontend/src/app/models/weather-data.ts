296d0dd2dec7cefb0173275b30ffbd9e573a5909
Ilaria <ila1402@icloud.com>
2024-12-16 16:16:55 +0100

    BEE-128 created weather data model frontend

---
+++ frontend/src/app/models/weather-data.ts
@@ -0,0 +1,13 @@
+import { precisionFixed } from "d3";
+
+export interface WeatherData{
+
+
+    localDataTime: Date;
+    temperature: number;
+    precipitation: number;
+    cloudCover: number;
+    windSpeed: number;
+
+    
+}
